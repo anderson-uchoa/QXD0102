c556a9ced7193c15bad06aea63cc1b61b3f784fd
allef <allef.lobo@gmail.com>
2015-12-18 14:21:40 -0300

    chat

--- app/src/main/java/quixada/ufc/br/kisan/activity/AddAnuncioActivity.java
+++ app/src/main/java/quixada/ufc/br/kisan/activity/AddAnuncioActivity.java
@@ -16,7 +16,7 @@ public class AddAnuncioActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
 
 
